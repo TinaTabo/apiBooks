60b2d29414103f0b9b6c1788bc7a77f9a7c211b5
Cristina Taboada <cris.taboada98@gmail.com>
2023-06-21 11:39:09 +0200

    estructura de la API completa

---
+++ src/database.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool(
+    {
+        host: "localhost",
+        user: "root",
+        password: "Secur3MyS4QL",
+        database: "appbooks",
+        waitForConnections: true,
+        connectionLimit: 10,
+        maxIdle: 10,
+        idleTimeout: 60000,
+        queueLimit: 0
+    }
+).promise();
+
+console.log("Conexión con la BBDD creada");
+
+module.exports = {pool};
---
+++ src/routers/myBooks.routers.js
@@ -0,0 +1,7 @@
+const { Router } = require('express');
+const router = Router();
+const myBooksCtrl =  require('../controller/myBooks.controller');
+
+
+
+module.exports = router;
