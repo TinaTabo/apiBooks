d3104ffe9b09b4893403349ebbb9315be89080fb
Cristina Taboada <cris.taboada98@gmail.com>
2023-06-23 10:52:22 +0200

    Preparación para despliegue en vercel

--- src/database.js
+++ src/database.js
@@ -2,10 +2,11 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool(
     {
-        host: "localhost",
-        user: "root",
-        password: "Secur3MyS4QL",
-        database: "appbooks",
+        host: process.env.DB_HOST || "localhost",
+        user: process.env.DB_USER || "root",
+        password: process.env.DB_PASSWORD || "",
+        database: process.env.DB_NAME || "appbooks",
+        port: process.env.DB_PORT || 3306,
         waitForConnections: true,
         connectionLimit: 10,
         maxIdle: 10,
